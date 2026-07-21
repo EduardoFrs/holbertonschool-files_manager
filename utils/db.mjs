637a9e5e31741802b676eea679a372dc2e8a30b0
EduardoFrs <fernandeseduardo00@gmail.com>
2026-07-21 09:27:07 +0200

    feat: 1. MongoDB utils

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,34 +1,40 @@
-import { MongoClient } from 'mongodb';
+import mongodb from 'mongodb';
+
+const { MongoClient } = mongodb;
+
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || 27017;
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-
-    this.client = new MongoClient(`mongodb://${host}:${port}`);
     this.db = null;
-
+    this.client = new MongoClient(`mongodb://${HOST}:${PORT}`, {
+      useUnifiedTopology: true,
+    });
     this.client.connect()
       .then(() => {
-        this.db = this.client.db(database);
+        this.db = this.client.db(DATABASE);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        console.error(`MongoDB client error: ${err.message}`);
+      });
   }
 
   isAlive() {
-    return !!this.db;
+    return this.db !== null;
   }
 
   async nbUsers() {
+    if (!this.isAlive()) return 0;
     return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
+    if (!this.isAlive()) return 0;
     return this.db.collection('files').countDocuments();
   }
 }
 
 const dbClient = new DBClient();
-
 export default dbClient;
